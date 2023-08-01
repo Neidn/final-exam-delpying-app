644de93f64347a107423d2c4a9381d7b53704f03
Neidn <57209608+Neidn@users.noreply.github.com>
2023-08-01 11:16:26 +0900

    modify views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-# <HINT> Import any new Models here
 from .models import *
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
@@ -111,14 +110,6 @@ def enroll(request, course_id):
     return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
 
 
-# <HINT> Create a submit view to create an exam submission record for a course enrollment,
-# you may implement it based on following logic:
-# Get user and course object, then get the associated enrollment object created when the user enrolled the course
-# Create a submission object referring to the enrollment
-# Collect the selected choices from exam form
-# Add each selected choice object to the submission object
-# Redirect to show_exam_result with the submission id
-# def submit(request, course_id):
 def submit(request, course_id):
     user = request.user
     course = get_object_or_404(Course, pk=course_id)
@@ -140,7 +131,6 @@ def submit(request, course_id):
     return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(course.id, submission.id,)))
 
 
-# <HINT> A example method to collect the selected choices from the exam form from the request object
 def extract_answers(request):
     submitted_answers = []
 
@@ -152,10 +142,6 @@ def extract_answers(request):
     return submitted_answers
 
 
-# <HINT> Create an exam result view to check if learner passed exam and show their question results and result for
-# each question, you may implement it based on the following logic: Get course and submission based on their ids Get
-# the selected choice ids from the submission record For each selected choice, check if it is a correct answer or not
-# Calculate the total score
 def show_exam_result(request, course_id, submission_id):
     course = get_object_or_404(Course, pk=course_id)
     submission = Submission.objects.get(pk=submission_id)
